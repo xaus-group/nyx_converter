e369b83a22eca695b8d7c9258368633092a29900
Hossein HassanNejad <hosseinspell@gmail.com>
2024-05-20 09:27:13 +0330

    video&audio codecs document

--- lib/src/helper/nyx_audio_codec.dart
+++ lib/src/helper/nyx_audio_codec.dart
@@ -1,4 +1,48 @@
-enum NyxAudioCodec { mp3, mp2, wma, aac, oog, flac, alac, dsd, pcmS16le }
+///
+/// ### Description:
+/// An audio codec, short for coder-decoder, is like a **miniaturization machine for sound**. It shrinks digital audio files by removing unnecessary information, allowing you to store and share music and audio more efficiently. This compression comes with a trade-off, though. While some codecs (like MP3) prioritize smaller file sizes, they might sacrifice some sound quality. Others (like FLAC) preserve the original quality but result in larger files.
+///
+/// Here's a quick breakdown:
+///
+/// - **Compresses audio:** Reduces file size for easier storage and transfer.
+/// - **Quality vs. Size:** Lossy codecs (MP3, AAC) prioritize size with some quality loss. Lossless codecs (FLAC) maintain quality but have larger files.
+/// - **Widely Used:** AAC is a popular choice for its balance of size and quality, found in MP4s, YouTube videos, and most devices.
+
+/// Choosing the right audio codec depends on your needs. If you prioritize pristine sound, go lossless. For smaller files and broader compatibility, consider a lossy codec with adjustable quality settings.
+///
+/// ### Example:
+/// ```dart
+/// final filePath = 'path/to/my.mp4';
+/// final outputPath = 'path/to/';
+///
+/// NyxConverter.convertTo(filePath, outputPath, audioCodec: NyxAudioCodec.mp3);
+/// ```
+///
+enum NyxAudioCodec {
+  /// The most widely used audio codec, known for its good balance between file size and sound quality. It's a lossy codec, meaning it discards some audio information during compression.A widely used lossy codec known for its good balance of quality and file size. However, some audio information is sacrificed during compression.
+  mp3,
+
+  /// Predecessor to MP3, offering lower quality but slightly smaller file sizes. Less common nowadays.
+  mp2,
+
+  /// Developed by Microsoft, WMA offers similar quality to MP3 at comparable bitrates. However, its compatibility is not as universal as MP3.
+  wma,
+
+  /// A modern and efficient lossy codec, generally considered to provide better audio quality than MP3 at similar bitrates. Widely used in MP4 files, YouTube videos, and most devices.
+  aac,
+
+  /// A free and open-source lossy codec known for its good quality and efficient compression. Often used in web applications and free software.
+  oog,
+
+  /// A popular lossless compression format that preserves the original audio quality without any information loss. FLAC files are typically larger than their lossy counterparts but ideal for archiving or high-fidelity audio.
+  flac,
+
+  /// Similar to FLAC, ALAC offers lossless compression but is primarily used by Apple devices and iTunes.
+  alac,
+
+  /// A high-resolution audio format that uses a different approach than traditional PCM (Pulse Code Modulation) codecs. DSD captures audio data in a more analog-like way, potentially offering very high fidelity, but requires specialized equipment for playback.
+  dsd
+}
 
 extension NyxAudioCodecCommandExtension on NyxAudioCodec {
   String get command {
@@ -19,8 +63,6 @@ extension NyxAudioCodecCommandExtension on NyxAudioCodec {
         return 'alas';
       case NyxAudioCodec.dsd:
         return 'dsd';
-      case NyxAudioCodec.pcmS16le:
-        return 'pcm_s16le';
     }
   }
 }
@@ -44,8 +86,6 @@ extension NyxAudioCodecTitleExtension on NyxAudioCodec {
         return 'Apple Lossless Audio Codec';
       case NyxAudioCodec.dsd:
         return 'Direct Stream Digital';
-      case NyxAudioCodec.pcmS16le:
-        return 'Pulse Code Modulation Signed 16-bit Little Endian';
     }
   }
 }
@@ -69,8 +109,6 @@ extension NyxAudioCodecNameExtension on NyxAudioCodec {
         return 'ALAC';
       case NyxAudioCodec.dsd:
         return 'DSD';
-      case NyxAudioCodec.pcmS16le:
-        return 'PCM S16LE';
     }
   }
 }
--- lib/src/helper/nyx_video_codec.dart
+++ lib/src/helper/nyx_video_codec.dart
@@ -1,4 +1,47 @@
-enum NyxVideoCodec { h264, h265, xvid, vp8, vp9, av1, mpeg4, mpeg2 }
+///
+/// ### Description:
+/// Video codecs, short for coder-decoders, are the unsung heroes that enable us to store and share videos efficiently. They act like **compression wizards**, shrinking massive video files by cleverly discarding redundant information while maintaining an acceptable level of visual quality.
+///
+/// Here's a quick breakdown:
+///
+/// - **Quality vs. File Size:** H.264 offers a good balance. H.265 provides higher compression but may require more processing power and have limited playback compatibility.
+/// - **Compatibility:** H.264 is the most widely supported choice.
+/// - **Application:** For web video, VP8/VP9 might be suitable.
+
+/// Video codecs are constantly evolving, with newer ones offering better compression and features. However, compatibility across devices and platforms remains a factor to consider.
+///
+/// ### Example:
+/// ```dart
+/// final filePath = 'path/to/my.mp4';
+/// final outputPath = 'path/to/';
+///
+/// NyxConverter.convertTo(filePath, outputPath, videoCodec: NyxVideoCodec.h264);
+/// ```
+enum NyxVideoCodec {
+  /// The current king of video codecs, offering a great balance between high quality and efficient compression. Widely supported by devices and platforms, making it a versatile choice.
+  h264,
+
+  /// The successor to H.264, boasting even better compression for smaller file sizes with similar quality. However, it requires more processing power for encoding and decoding, and playback compatibility is still catching up.
+  h265,
+
+  /// An open-source implementation of the MPEG-4 standard, known for its efficiency and compatibility with older devices. Not as widely used now as H.264 and H.265.
+  xvid,
+
+  /// Open-source codec developed by Google, often used for web video streaming like YouTube. They prioritize good compression for efficient delivery over the internet.
+  vp8,
+
+  /// Open-source codecs developed by Google, often used for web video streaming like YouTube. They prioritize good compression for efficient delivery over the internet.
+  vp9,
+
+  /// A newer royalty-free alternative to H.265, aiming for similar compression efficiency with lower processing demands. Still gaining adoption and may not be universally supported yet.
+  av1,
+
+  /// A broad term encompassing various standards, including H.264 (AVC). It can also refer to the MPEG-4 Part 2 video codec, a predecessor to H.264 but less efficient and not as widely used.
+  mpeg4,
+
+  /// An older video codec standard, commonly used for DVDs and broadcast television. It's not as efficient as newer codecs like H.264, resulting in larger file sizes for similar quality.
+  mpeg2
+}
 
 extension NyxVideoCodecCommandExtension on NyxVideoCodec {
   String get command {
