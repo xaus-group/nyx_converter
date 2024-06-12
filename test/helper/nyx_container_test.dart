617db2c24c93991b1961c99fb1f90c1f0c85e899
Hossein HassanNejad <hosseinspell@gmail.com>
2024-06-13 00:14:34 +0330

    nyx container test

---
+++ test/helper/nyx_container_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nyx_converter/nyx_converter.dart';
+
+void main() {
+  group('Nyx Container', () {
+    test('Containers should be String type', () {
+      bool actual = false;
+
+      for (var container in NyxContainer.values) {
+        if (container.command.runtimeType == String &&
+            container.name.runtimeType == String) {
+          actual = true;
+        }
+      }
+
+      expect(actual, true);
+    });
+
+    test('Containers should not contain space', () {
+      bool actual = false;
+
+      for (var container in NyxContainer.values) {
+        if (container.command.split(' ').length == 1) {
+          actual = true;
+        }
+      }
+
+      expect(actual, true);
+    });
+  });
+}
